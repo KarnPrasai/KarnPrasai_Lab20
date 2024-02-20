27eda9d2f3a80aebea2ac344284c7f6fd65a4ea6
KarnPrasai <karn_prasai@cmu.ac.th>
2024-02-20 13:41:45 +0700

    Lab20 completed

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -20,21 +20,85 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string& filename, vector<string>& names, vector<int>& scores, vector<char> &grades) {
+    ifstream file;
+    file.open(filename);
+    string textline;
+    while(getline(file,textline))
+    {
+        char format[] = "%[^:]: %d %d %d";
+        char name[50];
+        int a,b,c;
+        sscanf(textline.c_str(),format,name,&a,&b,&c);
+        names.push_back(name);
+        scores.push_back(a+b+c);
+        grades.push_back(score2grade(a+b+c));
+    }
+    file.close();
 }
 
-void getCommand(){
-
+void getCommand(string &command, string &key){
+    cout << "Please input your command: ";
+    cin >> command;
+    if(toUpperStr(command) == "NAME")
+    {
+        cin.ignore();
+        getline(cin,key);
+    }
+    else if(toUpperStr(command) == "GRADE")
+    {
+        cin >> key;
+    }
+    else if(toUpperStr(command) == "EXIT")
+    {
+    
+    }
 }
 
-void searchName(){
-
+void searchName(const vector<string>& names, const vector<int>& scores, const vector<char>& grades, const string& key) 
+{
+    bool namefounded = false;
+    cout << "---------------------------------\n";
+    for(unsigned int i = 0;i<names.size();i++)
+    {
+        string Name = toUpperStr(names.at(i));
+        if(Name == key)
+        {
+            cout << names.at(i) << "'s score = " << scores.at(i) << endl;
+            cout << names.at(i) <<"'s grade = " << grades.at(i) << endl;
+            namefounded = true;
+            break;
+        }
+    }
+    if(!namefounded)
+    {
+     cout << "Cannot found.\n";
+    }
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(const vector<string>& names, const vector<int>& scores, const vector<char>& grades, const string& key) 
+{
+    char Grade[2];
+    bool founded = false;
+    cout << "---------------------------------\n";
+    for(unsigned int i =0;i<grades.size();i++)
+    {
+        Grade[0] = grades.at(i);;
+        Grade[1] = '\0';
+        if(key == Grade)
+        {
+            cout << names.at(i) << " (" << scores.at(i) << ")" << endl;
+            founded = true;
+        }
+    }
+    if(!founded)
+    {
+        cout << "Cannot found.\n";
+    }
+    cout << "---------------------------------\n";
 }
+
 
 
 int main(){
